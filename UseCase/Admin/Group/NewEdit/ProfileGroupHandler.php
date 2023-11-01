36184dffc9a44e6a8a08935a83fecf6c77aeca13
kingstar <admin@baks.dev>
2023-11-01 14:59:43 +0300

    Baks Development

--- UseCase/Admin/Group/NewEdit/ProfileGroupHandler.php
+++ UseCase/Admin/Group/NewEdit/ProfileGroupHandler.php
@@ -45,7 +45,7 @@ final class ProfileGroupHandler extends AbstractHandler
 
         try
         {
-            $command->getEvent() ? $this->preUpdate($command) : $this->prePersist($command);
+            $command->getEvent() ? $this->preUpdate($command, true) : $this->prePersist($command);
         }
         catch(DomainException $errorUniqid)
         {
