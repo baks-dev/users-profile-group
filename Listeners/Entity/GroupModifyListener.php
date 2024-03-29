51839625715ad79103cb02eb3031a094ef763a4f
kingstar <admin@baks.dev>
2024-03-29 18:46:39 +0300

    Baks Development

--- Listeners/Entity/GroupModifyListener.php
+++ Listeners/Entity/GroupModifyListener.php
@@ -25,11 +25,13 @@ namespace BaksDev\Users\Profile\Group\Listeners\Entity;
 
 use BaksDev\Core\Type\Ip\IpAddress;
 use BaksDev\Users\Profile\Group\Entity\Modify\ProfileGroupModify;
+use BaksDev\Users\User\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\SwitchUserToken;
 
 #[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: ProfileGroupModify::class)]
 final class GroupModifyListener
@@ -50,9 +52,16 @@ final class GroupModifyListener
     {
         $token = $this->token->getToken();
 
-        if ($token)
-        {
+        if ($token) {
+
             $data->setUsr($token->getUser());
+
+            if($token instanceof SwitchUserToken)
+            {
+                /** @var User $originalUser */
+                $originalUser = $token->getOriginalToken()->getUser();
+                $data->setUsr($originalUser);
+            }
         }
 
         /* Если пользователь не из консоли */
