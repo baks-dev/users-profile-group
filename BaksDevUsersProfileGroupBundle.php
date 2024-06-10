ac4498cedced8646e91631d73b8578496b904354
kingstar <admin@baks.dev>
2024-06-10 10:43:34 +0300

    Baks Development

--- BaksDevUsersProfileGroupBundle.php
+++ BaksDevUsersProfileGroupBundle.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 
 namespace BaksDev\Users\Profile\Group;
 
+use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
 use DirectoryIterator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -23,4 +24,21 @@ class BaksDevUsersProfileGroupBundle extends AbstractBundle
     public const NAMESPACE = __NAMESPACE__.'\\';
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $services = $container->services()
+            ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+        $services->load(self::NAMESPACE, self::PATH)
+            ->exclude([
+                self::PATH.'{Entity,Resources,Type}',
+                self::PATH.'**/*Message.php',
+                self::PATH.'**/*DTO.php',
+            ]);
+
+        $services->set(UserProfileUid::class)->class(UserProfileUid::class);
+    }
 }
