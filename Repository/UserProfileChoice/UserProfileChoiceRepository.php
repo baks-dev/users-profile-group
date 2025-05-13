41242b51223a4ad7a59777cf201eb32af92245e5
MichelAngelo <baksdevelopmen@gmail.com>
2025-05-13 23:53:26 +0300

    BaksDevelopment

--- Repository/UserProfileChoice/UserProfileChoiceRepository.php
+++ Repository/UserProfileChoice/UserProfileChoiceRepository.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -103,7 +103,7 @@ final readonly class UserProfileChoiceRepository implements UserProfileChoiceInt
             )
             ->setParameter(
                 'account_status',
-                new EmailStatus(EmailStatusActive::class),
+                EmailStatusActive::class,
                 EmailStatus::TYPE
             );
 
